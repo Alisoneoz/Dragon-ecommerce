1eba9a91428a60ba92500edcd595d1aa9aec9940
Alison Estephany <alisonestephanyoz@gmail.com>
2023-03-14 23:29:06 -0400

    Add modal functionality to require user auth during checkout

---
+++ client/src/components/AuthenticationRequiredModal.jsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { AiFillCloseCircle } from "react-icons/ai";
+import logoDark from "../assets/logoDark.png";
+import logoLight from "../assets/logoLight.png";
+import { NavLink } from "react-router-dom";
+
+const AuthenticationRequiredModal = ({ closeModal }) => {
+  const userTheme = localStorage.getItem("theme");
+  return (
+    <div className="h-screen w-screen bg-black/70 fixed top-0 left-0 right-0 flex ease-in-out duration-500">
+      <div
+        className="ease-in-out duration-500 flex flex-col rounded-xl mx-auto my-auto shadow-2xl relative bg-primary-100 dark:bg-primary-900 dark:text-primary-100 
+       w-4/6 
+      sm:w-1/2 lg:h-4/6 py-4 sm:pb-0 px-5 "
+      >
+        <button onClick={()=>closeModal(false)} className="flex justify-end  text-4xl absolute right-5">
+          <AiFillCloseCircle />
+        </button>
+
+        <div className="px-3 flex flex-col justify-center ">
+          <img
+            src={userTheme === "dark" ? logoDark : logoLight}
+            className="w-40 flex justify-center mt-4 mx-auto mb-4 sm:mb-8 lg:mt-12"
+          />
+          <div className=" sm:px-0 flex flex-col items-center justify-center lg:px-16">
+            <div className="font-bold text-base sm:text-xl lg:text-2xl text-center leading-1 lg:mt-10 lg:mb-3">
+              Dear customer, to proceed with your checkout, please access to
+              your account.
+            </div>
+            <button className="w-full mt-3 sm:mt-4 py-3 px-5 rounded-md text-primary-100 bg-primary-900 dark-bg-primary-100 hover:bg-secondary-900 hover:dark:bg-secondary-100 hover:text-black font-bold lg:mb-5">
+              <NavLink to="/login">Log in</NavLink>
+            </button>
+
+
+
+            <div className="my-3  sm:mt-4 flex sm:justify-end items-center sm:pb-5">
+            <div className="flex">Don't have an account?</div>
+            <NavLink
+              to="/register"
+              className="ml-2 font-bold hover:text-xl min-w-fit sm:w-24 sm:hover:w-32 lg:hover:w-fit lg:w-fit  px-2 py-1 rounded-md border-2 border-primary-900 dark:border-primary-100"
+            >
+              Sign Up
+            </NavLink>
+          </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AuthenticationRequiredModal;
--- client/src/components/PayButton.jsx
+++ client/src/components/PayButton.jsx
@@ -1,35 +1,45 @@
-import axios from "axios"
-import {useSelector} from "react-redux";
-import { saveUser } from "../features/authSlice"
-
-import {auth} from "../config/FirebaseConfiguration";
-
-
+import { useState } from "react";
+import axios from "axios";
+import { auth } from "../config/FirebaseConfiguration";
+import AuthenticationRequiredModal from "./AuthenticationRequiredModal";
 
 const PayButton = ({ cartItems }) => {
-  //const user = useSelector((state)=>state.auth.value);
+  const [openModal, setOpenModal] = useState(false);
+
   const url = "https://dragon-ecommerce-backend.onrender.com";
   const user = auth.currentUser;
-  console.log(url)
-  const handleCheckOut = () =>{
-    console.log(user.uid)
-    
-    console.log(url)
-     axios.post(`${url}/stripe/create-checkout-session`, {cartItems, user: user.uid})
-   .then((res)=>{
-    if(res.data.url){
-      window.location.href= res.data.url
+  console.log(url);
+  const handleCheckOut = () => {
+    if (!user) {
+      setOpenModal(true);
+      return ;
+    } else {
+      console.log(user.uid);
+      axios
+        .post(`${url}/stripe/create-checkout-session`, {
+          cartItems,
+          user: user.uid,
+        })
+        .then((res) => {
+          if (res.data.url) {
+            window.location.href = res.data.url;
+          }
+        })
+        .catch((err) => console.log(err.message));
     }
-   })
-   .catch((err)=> console.log(err.message))
-  }
+  };
+  
   return (
-    <button 
-    onClick={handleCheckOut}
-    className="mt-4 py-3 px-5 rounded-md text-primary-100 bg-primary-900 dark-bg-primary-100 hover:bg-secondary-900 hover:dark:bg-secondary-100 opacity-100 hover:text-black font-bold">
-    Check Out
-  </button>
-  )
-}
+<div className="">
+<button
+      onClick={handleCheckOut}
+      className={!openModal ?"mt-4 py-3 px-5 rounded-md text-primary-100 bg-primary-900 dark-bg-primary-100 hover:bg-secondary-900 hover:dark:bg-secondary-100 opacity-100 hover:text-black font-bold" : "h-min-screen w-screen "}
+    >
+      Check Out
+    </button>
+    {openModal && (<AuthenticationRequiredModal closeModal={setOpenModal}/>)}
+</div>
+  );
+};
 
-export default PayButton
+export default PayButton;
